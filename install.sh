2086b6ba3ed279fe9621927906042e237839533b
Jeffrey Manuel <87138660+Jeffrey2081@users.noreply.github.com>
2025-02-01 12:20:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,9 +19,9 @@ sudo dnf install -y ./rpm-packages/ocs-url-3.1.0-1.fc20.x86_64.rpm
 # Installing fonts
 sudo dnf install -y fontawesome-fonts fontawesome-fonts-web
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip FiraCode.zip -d /usr/share/fonts
+sudo unzip FiraCode.zip -d /usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-unzip Meslo.zip -d /usr/share/fonts
+sudo unzip Meslo.zip -d /usr/share/fonts
 # Reloading Font
 fc-cache -vf
 # Removing zip Files
